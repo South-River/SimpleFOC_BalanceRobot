9cef41875709cafce954f1ceda8ed1f91630f089
SouthRiver <52597530+South-River@users.noreply.github.com>
2022-06-09 02:22:16 +0800

    Update Output.cpp

--- 2.Firmware/Controller/Output.cpp
+++ 2.Firmware/Controller/Output.cpp
@@ -2,33 +2,45 @@
 
 void printRPY(const IMU::IMU &imu)
 {
-  Serial.print("roll: ");  Serial.print(imu.roll*IMU::RAD2ANG);  Serial.print("\t");
-  Serial.print("pitch: "); Serial.print(imu.pitch*IMU::RAD2ANG); Serial.print("\t");
-  Serial.print("yaw: ");   Serial.print(imu.yaw*IMU::RAD2ANG);   Serial.print("\t");
-  Serial.print("\n");
+  String str="";
+  
+  str+=("roll: "+String(imu.roll*IMU::RAD2ANG,3)+"\t");
+  str+=("pitch: "+String(imu.pitch*IMU::RAD2ANG,3)+"\t");
+  str+=("yaw: "+String(imu.yaw*IMU::RAD2ANG,3)+"\t");
+  
+  Serial.println(str);
 }
 
 void printQuaternion(const IMU::IMU &imu)
 {
-  Serial.print("w: "); Serial.print(imu.q[0]); Serial.print("\t");
-  Serial.print("x: "); Serial.print(imu.q[1]); Serial.print("\t");
-  Serial.print("y: "); Serial.print(imu.q[2]); Serial.print("\t");
-  Serial.print("z: "); Serial.print(imu.q[3]); Serial.print("\t");
-  Serial.print("\n");
+  String str="";
+  
+  str+=("w: "+String(imu.q[0],3)+"\t");
+  str+=("x: "+String(imu.q[1],3)+"\t");
+  str+=("y: "+String(imu.q[2],3)+"\t");
+  str+=("z: "+String(imu.q[3],3)+"\t");
+  
+  Serial.println(str);
 }
 
 void printAcc(const IMU::IMU &imu)
 {
-  Serial.print("ax: "); Serial.print(imu.accel[0]); Serial.print("\t");
-  Serial.print("ay: "); Serial.print(imu.accel[1]); Serial.print("\t");
-  Serial.print("az: "); Serial.print(imu.accel[2]); Serial.print("\t");
-  Serial.print("\n");
+  String str="";
+  
+  str+=("ax: "+String(imu.accel[0],3)+"\t");
+  str+=("ay: "+String(imu.accel[1],3)+"\t");
+  str+=("az: "+String(imu.accel[2],3)+"\t");
+  
+  Serial.println(str);
 }
 
 void printGyro(const IMU::IMU &imu)
 {
-  Serial.print("gx: "); Serial.print(imu.gyro[0]*IMU::RAD2ANG); Serial.print("\t");
-  Serial.print("gy: "); Serial.print(imu.gyro[1]*IMU::RAD2ANG); Serial.print("\t");
-  Serial.print("gz: "); Serial.print(imu.gyro[2]*IMU::RAD2ANG); Serial.print("\t");
-  Serial.print("\n");
+  String str="";
+  
+  str+=("gx: "+String(imu.gyro[0]*IMU::RAD2ANG,3)+"\t");
+  str+=("gy: "+String(imu.gyro[1]*IMU::RAD2ANG,3)+"\t");
+  str+=("gz: "+String(imu.gyro[2]*IMU::RAD2ANG,3)+"\t");
+  
+  Serial.println(str);
 }
